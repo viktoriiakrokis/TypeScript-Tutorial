14ec9c1e22d523f31ed6b09047431b6fe545fbf4
viktoriiakrokis <vika.krokis063@gmail.com>
2020-02-28 17:34:34 +0200

    Initial commit

---
+++ tsc/example.ts
@@ -0,0 +1,12 @@
+class Example {
+
+
+    public house = null;
+
+    run() {
+        alert('hello')
+    }
+
+}
+
+console.log("hello");
---
+++ tsc/type_inference.ts
@@ -0,0 +1,17 @@
+
+let car = {
+
+    brand: "BMW",
+    engine: 3.4,
+    run: function(){
+
+        console.log("driving at 100 miles an hour")
+
+    }
+
+}
+
+car.brand = 5.4; // it is string-value property, i cant assign number value
+
+
+
